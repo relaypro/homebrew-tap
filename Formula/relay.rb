6c25d14866ff66f6c878e23c37524f301e77ba95
Brandon Smith <bsmith@republicwireless.com>
2021-03-03 00:20:25 -0500

    Update relay.rb

--- Formula/relay.rb
+++ Formula/relay.rb
@@ -4,11 +4,11 @@ class Relay < Formula
   homepage "https://api-docs.relaypro.com/"
   url "https://relaygo.link/packages/relay-v0.0.1/relay-v0.0.1.tar.gz"
   sha256 "556b2764c39cd371444b64fe39ceed9d591eb3fd4c7f9efa1550cebec1af50cc"
-  depends_on "heroku/brew/heroku-node"
+  depends_on "node@14"
 
   def install
     inreplace "bin/relay", /^CLIENT_HOME=/, "export RELAY_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
-    inreplace "bin/relay", "\"$DIR/node\"", "#{Formula["heroku-node"].opt_share}/node"
+    inreplace "bin/relay", "\"$DIR/node\"", "#{Formula["node@14"].bin}/node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/relay"
   end
