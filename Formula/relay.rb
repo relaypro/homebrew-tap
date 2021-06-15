a885495c775ef012b9e5e62ab9442507d80d99ac
Brandon Smith <brandon@fieldsignature-mbp.local>
2021-06-15 18:50:51 -0400

    Bump to v0.1.1

--- Formula/relay.rb
+++ Formula/relay.rb
@@ -1,9 +1,9 @@
 class Relay < Formula
   desc "Get started developing Relay Workflows"
-  version "0.0.4"
+  version "0.1.0"
   homepage "https://api-docs.relaypro.com/"
-  url "https://relaygo.link/packages/relay-v0.0.4/relay-v0.0.4.tar.gz"
-  sha256 "5e0c2ebeb9cfe9f7b59e4d21a3d6beea53898d7848b3909d53b0453e3cc8a1f0"
+  url "https://relaygo.link/packages/relay-v0.1.1/relay-v0.1.1.tar.gz"
+  sha256 "e59056f40c609776bef5b1f26c68c89f6fcf96175e9ef187fea5cc2eddc37fc0"
   depends_on "node@14"
 
   def install
